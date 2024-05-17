0152d1953e8a1b0ffef1c5c1c714f20ae50acc2b
abhigithub1991 <168497962+abhigithub1991@users.noreply.github.com>
2024-05-17 02:09:43 -0700

    Delete scripts/scripts directory

--- scripts/scripts/scripts/start_container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Pull the Docker image from Docker Hub
-docker pull abhinav1991/simple-python-app-latest
-
-# Run the Docker image as a container
-docker run -d -p 5000:5000 abhinav1991/simple-python-app-latest
